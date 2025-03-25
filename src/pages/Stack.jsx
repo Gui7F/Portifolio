d6bb9c68333b8778ed1de47210e8a47bbaa3c17a
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2025-03-25 08:35:59 +0000

    add: add icons on stack section

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import { DeviconJavascript } from '../components/Icons'
+import { DeviconReactnative } from '../components/Icons'
+import { DeviconMongodb } from '../components/Icons'
+import { DeviconNodejs } from '../components/Icons'
+import { DeviconNpm } from '../components/Icons'
+import { DeviconTypescript } from '../components/Icons'
+import { DeviconVitejs } from '../components/Icons'
+import { DeviconExpress } from '../components/Icons'
+import { DeviconTailwindcss } from '../components/Icons'
+import { DeviconPrisma } from '../components/Icons'
+import { DeviconVscode } from '../components/Icons'
+import { DeviconGit } from '../components/Icons'
 const Stack = () => {
   return (
-    <div>Stack</div>
+    <div>
+      <DeviconJavascript icon="devicon:javascript" width="128" height="128" />
+      <DeviconReactnative  icon="devicon:reactnative" width="128" height="128"/>
+      <DeviconMongodb icon="devicon:mongodb" width="128" height="128"/>
+      <DeviconNodejs icon="devicon:nodejs" width="128" height="128"/>
+      <DeviconNpm icon="devicon:npm" width="128" height="128"/>
+      <DeviconTypescript icon="devicon:npm" width="128" height="128"/>
+      <DeviconVitejs icon="devicon:vite" width="128" height="128"/>
+      <DeviconExpress icon="devicon:express" width="128" height="128"/>
+      <DeviconTailwindcss icon="devicon:tailwind" width="128" height="128"/>
+      <DeviconPrisma icon="devicon:prisma" width="128" height="128"/>
+      {/* <DeviconVscode icon="devicon:vscode" width="128" height="128"/> */}
+      <DeviconGit icon="devicon:prisma" width="128" height="128"/>
+    </div>
   )
 }
 
