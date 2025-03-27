d6901f28d43bf889d0328d774d46d2cef0d21e1f
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2025-03-27 02:09:51 +0000

    feat: animation on stack section

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
-// import { ScrollTrigger } from "gsap/all";
+import { ScrollTrigger } from "gsap/all";
 // icons
 import {
   DeviconJavascript,
@@ -21,18 +21,21 @@ import {
 } from "../components/Icons";
 
 const Stack = () => {
-  
   const containerRef = useRef(null);
-  
-  gsap.registerPlugin(useGSAP)
-  // gsap.registerPlugin(ScrollTrigger)
-  
+  const jsRef = useRef(null);
+  gsap.registerPlugin(useGSAP);
+  gsap.registerPlugin(ScrollTrigger);
+
   const { contextSafe } = useGSAP({ scope: containerRef });
 
-  const onClickAnimated = contextSafe(() =>{
-     gsap.from(".box", {rotate : 360})
-     gsap.from(".box2", {y : 100})
-  })
+  const onClickAnimated = contextSafe(() => {
+    gsap.to(".box1", {
+      duration: 1,
+      rotate: 360,
+      ease:"bounce",
+    })
+  });
+
 
   return (
     <div
@@ -42,25 +45,94 @@ const Stack = () => {
       }}
       ref={containerRef}
     >
-      <div className="grid grid-cols-4 grid-rows-3 gap-y-16 place-items-center" >
-        <DeviconJavascript icon="devicon:javascript" width="128" height="128" className="box" onClick={onClickAnimated} />
+      <div className="grid grid-cols-4 grid-rows-3 gap-y-16 place-items-center">
+        <DeviconJavascript
+          icon="devicon:javascript"
+          width="128"
+          height="128"
+          className="box1"
+          onClick={onClickAnimated}
+          ref={jsRef}
+        />
+
         <DeviconReactnative
           icon="devicon:reactnative"
           width="128"
           height="128"
           className="box2"
-          onClick = {onClickAnimated}
+          onClick={onClickAnimated}
         />
-        <DeviconMongodb icon="devicon:mongodb" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconNodejs icon="devicon:nodejs" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconNpm icon="devicon:npm" width="128" height="128" className='box'  onClick = {onClickAnimated}/>
-        <DeviconTypescript icon="devicon:npm" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconVitejs icon="devicon:vite" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconExpress icon="devicon:express" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconTailwindcss icon="devicon:tailwind" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconPrisma icon="devicon:prisma" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconVscode icon="devicon:vscode" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
-        <DeviconGit icon="devicon:prisma" width="128" height="128" className="box"  onClick = {onClickAnimated}/>
+
+        <DeviconMongodb
+          icon="devicon:mongodb"
+          width="128"
+          height="128"
+          className="box3"
+          onClick={onClickAnimated}
+        />
+        <DeviconNodejs
+          icon="devicon:nodejs"
+          width="128"
+          height="128"
+          className="box4"
+          onClick={onClickAnimated}
+        />
+        <DeviconNpm
+          icon="devicon:npm"
+          width="128"
+          height="128"
+          className="box5"
+          onClick={onClickAnimated}
+        />
+        <DeviconTypescript
+          icon="devicon:npm"
+          width="128"
+          height="128"
+          className="box6"
+          onClick={onClickAnimated}
+        />
+        <DeviconVitejs
+          icon="devicon:vite"
+          width="128"
+          height="128"
+          className="box7"
+          onClick={onClickAnimated}
+        />
+        <DeviconExpress
+          icon="devicon:express"
+          width="128"
+          height="128"
+          className="box8"
+          onClick={onClickAnimated}
+        />
+        <DeviconTailwindcss
+          icon="devicon:tailwind"
+          width="128"
+          height="128"
+          className="box9"
+          onClick={onClickAnimated}
+        />
+        <DeviconPrisma
+          icon="devicon:prisma"
+          width="128"
+          height="128"
+          className="box10"
+          onClick={onClickAnimated}
+        />
+        <DeviconVscode
+          icon="devicon:vscode"
+          width="128"
+          height="128"
+          className="box11"
+          onClick={onClickAnimated}
+        />
+        <DeviconGit
+          icon="devicon:prisma"
+          width="128"
+          height="128"
+          className="box12"
+          onClick={onClickAnimated}
+        />
       </div>
     </div>
   );
