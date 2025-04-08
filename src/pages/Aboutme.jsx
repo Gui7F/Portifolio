e79afd12c55d6af2d5b7b5a1b17584020086b449
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2025-04-08 03:00:38 +0000

    chore: add text for about me section

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -4,7 +4,7 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
-// Components 
+// Components
 import ShinyText from "../components/ShinyText";
 
 const Aboutme = () => {
@@ -15,12 +15,14 @@ const Aboutme = () => {
     <div className="2xl:flex 2xl:space-x-20  m-auto relative bg-black 2xl:w-3/4 w-[90%] border border-neutral-600 pt-[80px] pb-[80px]">
       {/* Imagem */}
       <div className="2xl:w-[40%] w-[80%] m-auto h-[500px]  bg-slate-400">
-        <img src="./fotoperfil.jpg" alt="" />
+        <img className="h-full w-full object-cover" src="./fotoperfil.jpg" alt="" />
       </div>
 
       {/* Texto com Carrossel */}
       <div className="2xl:w-1/2 text-slate-300 relative">
-        <h1 className='2xl:text-5xl md:mt-5 max-[480px]:mt-9 text-3xl text-center'><ShinyText text={"Sobre Mim"}/></h1>
+        <h1 className="2xl:text-5xl md:mt-5 max-[480px]:mt-9 text-3xl text-center">
+          <ShinyText text={"Sobre Mim"} />
+        </h1>
 
         {/* Botões personalizados */}
         <button
@@ -54,18 +56,67 @@ const Aboutme = () => {
         >
           <SwiperSlide>
             <div>
-              <h2 className="text-xl text-center font-bold mb-2">Texto1</h2>
-              <p >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, eos amet? Delectus fuga voluptate perspiciatis suscipit, odio modi possimus sapiente, obcaecati, ratione consequuntur quasi nisi dignissimos quibusdam nihil necessitatibus repellendus.
+              <h2 className="text-xl text-center font-bold mb-2">
+                Apresentação
+              </h2>
+              <p className="mb-2">
+                Meu nome é Guilherme Ribeiro, sou estudante de Analise e
+                Desenvolvimento de Sistemas. Tenho 25 anos e sou apaixonado por
+                tecnologia, programação e design. Estou sempre em busca de novos
+                desafios e oportunidades para aprender e crescer na área de
+                desenvolvimento web.
+              </p>
+              <p>
+                Sou pai de duas crianças e sou casado, o que me motiva a
+                trabalhar duro para proporcionar um futuro melhor para minha
+                família. Acredito que a tecnologia pode transformar vidas e
+                estou comprometido em fazer parte dessa transformação.
               </p>
             </div>
           </SwiperSlide>
 
           <SwiperSlide>
-          <div>
-              <h2 className="text-xl text-center font-bold mb-2">Texto1</h2>
+            <div>
+              <h2 className="text-xl text-center font-bold mb-2">
+                Experiência
+              </h2>
+              <p className="mb-2">
+                Trabalho desde os 16 anos, quando iniciei minha trajetória como
+                jovem aprendiz na área de vendas. Com o tempo, fui ampliando
+                minha experiência em diferentes setores, atuando como ajudante
+                até chegar a Encanador Industrial II na área de mecânica
+                industrial e, atualmente, como vigilante patrimonial. Essa
+                jornada tem sido essencial para meu desenvolvimento pessoal e
+                profissional, permitindo que eu conclua meus estudos e invista
+                na minha formação superior.
+              </p>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, eos amet? Delectus fuga voluptate perspiciatis suscipit, odio modi possimus sapiente, obcaecati, ratione consequuntur quasi nisi dignissimos quibusdam nihil necessitatibus repellendus.
+                Paralelamente, venho me dedicando a projetos práticos e
+                freelancers na área de desenvolvimento web, onde tenho aplicado
+                meus conhecimentos em situações reais com clientes e demandas
+                específicas. Estou constantemente me aprimorando, com o objetivo
+                de ingressar de forma sólida e preparada no mercado profissional
+                de tecnologia.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div>
+              <h2 className="text-xl text-center font-bold mb-2">
+                Personalidade
+              </h2>
+              <p className="mb-2">
+                Sou uma pessoa simples, que valoriza o caráter e a bondade.
+                Procuro ser justo e honesto em todas as situações. Gosto de
+                ajudar os outros e estou sempre disposto a aprender com quem me
+                estende a mão. Sou extremamente colaborativo, pois acredito que
+                ninguém realiza nada sozinho. Por isso, levo a humildade e o
+                respeito como princípios fundamentais da minha vida.
+              </p>
+              <p>
+                Gosto de paz e tranquilidade desde mais novo, nunca fui de ir muito a festas ou baladas. Prefiro ficar em casa, jogando videogame ou assistindo
+                filmes e séries no meu tempo livre ou simplemente passear com a familia e amigos, aproveitando momentos de descontração e diversão. Tambem gosto 
+                de treinar e cuidar da minha saúde, e me manter em forma, tanto fisicamente quanto mentalmente.
               </p>
             </div>
           </SwiperSlide>
